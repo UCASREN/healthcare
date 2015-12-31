e3002d7ca5fa8a3aeeef2ec5382cec53fe18ee56
nijiazhi <nijiazhi@hrbeu.edu.cn>
2015-12-31 11:33:28 +0800

    哪里改了，我也不知到啊

--- src/main/java/otc/healthcare/dao/DataSourceFactory.java
+++ src/main/java/otc/healthcare/dao/DataSourceFactory.java
@@ -11,14 +11,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.ContextLoader;
 
 public class DataSourceFactory {
-	private static Map<String,DataSource> dataSourceMap = new HashMap<String,DataSource>();
-	public static Connection getConnection(){
+	private static Map<String, DataSource> dataSourceMap = new HashMap<String, DataSource>();
+
+	public static Connection getConnection() {
 		return getConnection(null);
 	}
-	public static Connection getConnection(String dataSourceName){
-		if(dataSourceName==null)
-			dataSourceName="dataSource_basic";
-		if(!dataSourceMap.containsKey(dataSourceName)){
+
+	public static Connection getConnection(String dataSourceName) {
+		if (dataSourceName == null)
+			dataSourceName = "dataSource_basic";
+		if (!dataSourceMap.containsKey(dataSourceName)) {
 			cacheDataSource(dataSourceName);
 		}
 		try {
@@ -29,12 +31,13 @@ public class DataSourceFactory {
 		}
 		return null;
 	}
-	private static void cacheDataSource(String dataSourceName){
-		if(!dataSourceMap.containsKey(dataSourceName)){
-			ApplicationContext ctx=ContextLoader.getCurrentWebApplicationContext();
-			DataSource dataSource=ctx.getBean(dataSourceName,DataSource.class);
+
+	private static void cacheDataSource(String dataSourceName) {
+		if (!dataSourceMap.containsKey(dataSourceName)) {
+			ApplicationContext ctx = ContextLoader.getCurrentWebApplicationContext();
+			DataSource dataSource = ctx.getBean(dataSourceName, DataSource.class);
 			dataSourceMap.put(dataSourceName, dataSource);
-			System.out.println("缓存数据源:"+dataSourceName);
+			System.out.println("缓存数据源:" + dataSourceName);
 		}
 	}
 }
